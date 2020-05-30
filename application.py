446e2b47ac9dfa7b8cf1355b2f69a175ec8ebee2
Glenn Langdon <glennlangdon@gmail.com>
2020-05-30 15:16:14 -0600

    Basic styling completed

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
