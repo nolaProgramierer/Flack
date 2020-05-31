3d27804e7a057b07dc72762eaca6a891d3ec571f
Glenn Langdon <glennlangdon@gmail.com>
2020-05-31 00:31:45 -0600

    JS for users and channel listing basically workingg

--- static/js/index.js
+++ static/js/index.js
@@ -6,25 +6,34 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelector('#user-form').onsubmit = function() {
         let name = document.querySelector('#user-name').value;
         localStorage.setItem('name', name);
-        document.querySelector('#current-user').innerHTML = localStorage.getItem('name')
+        document.querySelector('#current-user').innerHTML = localStorage.getItem('name');
+        document.querySelector('#user-name').value = '';
       } //onsubmit
     return false;
   } else {
-    // Load current value of localStorage
-    document.querySelector('#current-user').innerHTML = localStorage.getItem('name')
-    //localStorage.clear();
-    return false;
+      var currentUser = localStorage.getItem('name');
+      document.querySelector('#user-form').onsubmit = function() {
+      localStorage.removeItem(currentUser);
+      let name = document.querySelector('#user-name').value;
+      localStorage.setItem('name', name);
+      var currentUser = localStorage.getItem('name');
+      //document.querySelector('#current-user').innerHTML = currentUser;
+      // Load current value of localStorage
+      document.querySelector('#current-user').innerHTML = "Welcome " + currentUser + "!";
+      document.querySelector('#user-name').value = '';
+      //localStorage.clear();
+      return false;
     }
   }
-);
-// Add new channel to ul
-document.addEventListener('DOMContentLoaded', function() {
+  //Add new channels
   document.querySelector('#channel-form').onsubmit = function() {
     const li = document.createElement('li');
     li.setAttribute('class', 'channel')
     li.innerHTML = document.querySelector('#channel-name').value;
     document.querySelector('#channel-item').append(li);
+
     document.querySelector('#channel-name').value = '';
+
     // make channels clickable; break out into separate function
     var listChannels = document.getElementsByClassName('channel');
     for (var i = 0; i < listChannels.length; i++) {
@@ -33,28 +42,6 @@ document.addEventListener('DOMContentLoaded', function() {
         alert("Listchannel alert")
       });
     }
-
     return false;
-  }
+  };
 });
-
-
-// Add channel to local storage if channel does not exist
-document.addEventListener('DOMContentLoaded', function() {
-
-  if (!localStorage.getItem('channel')) {
-    document.querySelector('#current-user').innerHTML = "Please sign in";
-      document.querySelector('#user-form').onsubmit = function() {
-        let name = document.querySelector('#user-name').value;
-        localStorage.setItem('name', name);
-        document.querySelector('#current-user').innerHTML = localStorage.getItem('name')
-      } //onsubmit
-    return false;
-  } else {
-    // Load current value of localStorage
-    document.querySelector('#current-user').innerHTML = localStorage.getItem('name')
-    //localStorage.clear();
-    return false;
-    }
-  }
-);
